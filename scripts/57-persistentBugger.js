3df9333f6ef142d66d1447e53fc70368ca2ab1b2
Pawel Skwirowski <pskwirowski@gmail.com>
2022-12-29 14:01:18 +0100

    Add kata Persistent Bugger 6kyu

---
+++ scripts/57-persistentBugger.js
@@ -0,0 +1,32 @@
+// ? Exercise from CodeWars: Persistent Bugger, rank 6kyu
+/**
+ * Write a function that takes in a positive parameter num and returns its multiplicative persistence,
+ * which is the number of times you must multiply the digits in num until you reach a single digit.
+ *
+ * 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+ * 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+ * 4 --> 0 (because 4 is already a one-digit number)
+ */
+
+function persistence(num) {
+  let count = 0;
+
+  if (num < 10) {
+    return count;
+  }
+
+  const sum = num
+    .toString()
+    .split("")
+    .reduce((acc, curr) => +acc * +curr);
+
+  if (sum >= 10) {
+    count = Math.max(count, persistence(sum));
+  }
+
+  count += 1;
+
+  return count;
+}
+
+console.log(persistence(25));
